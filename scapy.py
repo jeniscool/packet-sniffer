ace83e9163e4dd76d3762d13da0d8a40cc3e3727
jeniscool <jmle265@g.uky.edu>
2019-04-08 14:44:54 -0400

    initializing

---
+++ scapy.py
@@ -0,0 +1,19 @@
+from scapy.all import *
+import sys
+import socket 
+import os
+    
+def fields_extraction(x):
+    print x.sprintf("{IP:%IP.src%,%IP.dst%,}"
+        "{TCP:%TCP.sport%,%TCP.dport%,}"
+        "{UDP:%UDP.sport%,%UDP.dport%}")
+
+    # print x.summary()
+    
+    # x.show()
+
+    #use x.time for time information on the pkts
+
+pkts = sniff(prn = fields_extraction, count = 10)
+
+# print pkts[0].show()
