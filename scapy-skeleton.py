9325f71c8c7701d2a5bf4de23601ccae88a0b65c
jeniscool <jmle265@g.uky.edu>
2019-04-10 22:04:04 -0400

    grouping packets with same flow together

--- scapy-skeleton.py
+++ scapy-skeleton.py
@@ -8,9 +8,8 @@ import numpy as np
 from collections import Counter
 import csv
 
-
 # **********************************
-# Classes to store packet data
+# Classes to store packet data - don't delete yet
 # **********************************
 #   - Ethernet header: dst, src, type;
 class Ethernet:
@@ -28,7 +27,7 @@ class IP:
         self.v = v
         self.ihl = ihl
         self.tos = tos
-        self.len = len
+        self.len = len # average size of exchanged packets??
         self.id = id
         self.flags = flags
         self.frag = frag
@@ -39,7 +38,12 @@ class IP:
         self.dest = dest
 
 
-#   - TCP header: sport, dport, seq, ack, dataofs, reserved, flags, window, chksum, urgptr, options;
+# ****** TCP header: sport, dport, seq, ack, dataofs, reserved, flags, window, chksum, urgptr, options ******
+# Used for reliable connections•
+# Checks for: lost packets, transmission errors, packets out of order and so on....
+# examples: emails, sms, internet browsing
+# Transportation Layer
+# ****************************************
 class TCP:
 
     def __init__(self, sport, dport, seq, ack, dataofs, reserved, flags, window, chksum, urgptr, options):
@@ -56,7 +60,13 @@ class TCP:
         self.options = options
 
 
-#   - UDP header: sport, dport, len, chksum;
+# ****** UDP header: sport, dport, len, chksum ******
+# Used for unreliable connections with no sessions
+# Does not check for error checking or flow control
+# Sends packets and forgets
+# examples:voice over IP, video streaming
+# Transportation Layer
+# ***************************************************
 class UDP:
 
     def __init__(self, sport, dport, len, chksum):
@@ -68,40 +78,47 @@ class UDP:
 #   - Occasionally some other protocol might show up, such as ARP, which is a transport layer protocol.
 
 
-flow1 = []
-flow2 = []
-flow3 = []
-flow4 = []
-flow5 = []
-flow6 = []
-flow7 = []
-flow8 = []
-
 # **********************************
 # Function to pull data from packets
 # **********************************
 def fields_extraction(packet):
-    for i in range(8):
-        if not flow1:
-
-
-
-    pEther.append(packet)
-
 
     # Fill classes
-#    pEther[sum(cPackets.values())] = Ethernet(packet[0][0].dst, packet[0][0].src, hex(packet[0][0].type))
-#    pIP[sum(cPackets.values())] = IP(packet[0][1].version, packet[0][1].ihl, packet[0][1].tos, packet[0][1].len,
-#                                     packet[0][1].id, packet[0][1].flags, packet[0][1].frag, packet[0][1].ttl,
-#                                     packet[0][1].proto, packet[0][1].chksum, packet[0][1].src, packet[0][1].dst)
-#
-#    pTCP[sum(cPackets.values())] = TCP(packet[0][2].sport, packet[0][2].dport, packet[0][2].seq, packet[0][2].ack,
-#                                       packet[0][2].dataofs, packet[0][2].reserved, packet[0][2].flags,
-#                                       packet[0][2].window, packet[0][2].chksum, packet[0][2].urgptr,
-#                                       packet[0][2].options)
+    # pEther[sum(cPackets.values())] = Ethernet(packet[0][0].dst, packet[0][0].src, hex(packet[0][0].type))
+    # pIP[sum(cPackets.values())] = IP(packet[0][1].version, packet[0][1].ihl, packet[0][1].tos, packet[0][1].len,
+    #                                 packet[0][1].id, packet[0][1].flags, packet[0][1].frag, packet[0][1].ttl,
+    #                                 packet[0][1].proto, packet[0][1].chksum, packet[0][1].src, packet[0][1].dst)
+    #
+    # pTCP[sum(cPackets.values())] = TCP(packet[0][2].sport, packet[0][2].dport, packet[0][2].seq, packet[0][2].ack,
+    #                                   packet[0][2].dataofs, packet[0][2].reserved, packet[0][2].flags,
+    #                                   packet[0][2].window, packet[0][2].chksum, packet[0][2].urgptr,
+    #                                   packet[0][2].options)
 
     # use packet.time for time information on the pkts
+    packet.show()
 
+# **********************************
+# Function to find flows
+# **********************************
+def find_flows():
+    # for all packets found, group them into flows
+    for packet in packets:
+        # a flow is a tuple consisting of: [srcIP addr, srcport, destIP addr, destport, tranproto]
+        # assign to each flow an ID, which is shared among all packets of that flow.
+        ID = (packet[0][1].src, packet[0][2].sport, packet[0][1].dst, packet[0][2].dport, packet[0][1].proto)
+
+        if ID in flows:  # append packet to flow, if already exists
+            flows[ID].append(packet)
+        else:  # else check for bi-directional flows
+            # represents the bi-directional flow
+            bi_ID = (packet[0][1].dst, packet[0][2].dport, packet[0][1].src, packet[0][2].sport, packet[0][1].proto)
+            if bi_ID in flows:  # if that's in the packet, append
+                flows[bi_ID].append(packet)
+            else:  # if not, create a new flow in dictionary
+                flows[ID] = [packet]
+
+    # ****** DELETE LATER ******
+    print(f'Number of flows: {len(flows)}')
 
 # **********************************
 # Function to fill csv with packet data
@@ -112,43 +129,31 @@ def fields_extraction(packet):
         # pd-writer = csv.writer(pd, deliminator = ',', quotechar = '"')
 #        fieldNames = ['flow_id', 'feature_1', 'feature_2', 'feature_3', 'feature_4', 'label']
 
-# create packet counter
-# cPackets = Counter()
 
-# variable for count
-c = 1000
+# *********************************
+# Main function!
+# *********************************
+def main():
+    # Step 1: Extract all packets that belong to the same flow
+    # tuple consisting of: [srcIP addr, srcport, destIP addr, destport, tranproto]
+    find_flows()
 
-# Use sniff function to start sniffing packets.
-pkts = sniff(prn=fields_extraction, count=c)
-#pkts = sniff(count=c)
-
-
-tempProto = pkts[0][1].proto
-tempSrc = pkts[0][1].src
-tempDst = pkts[0][1].dst
-tempSport = pkts[0][2].sport
-tempDport = pkts[0][2].dport
-
-for i in range(c):
-    # print(pkts[i][1].proto)
-    if pkts[i][1].proto == tempProto and pkts[i][1].src == tempSrc and \
-            pkts[i][1].dst == tempDst and pkts[i][2].sport == tempSport and pkts[i][2].dport == tempDport:
-        print("hi")
+    # Step 2: Extract the interested value from each packet of the flow
+    # and calculate a statistical measure (max, min, avg, std_dev...)
 
 
 
+# *********************************
+# Global Variables
+# ********************************
+cPackets = Counter() # packet counter
+c = 50 # variable for amount of packets to collect
+# packets = sniff(prn=fields_extraction, count=c) # sniffed packets
+packets = sniff(count = c)
+# might need numpy arrays to hold features in classes
+flows = {}  # dictionary to hold flows
 
-
-#pEther[0].show()
-
-# fill_csv()
-
-# print(pEther)
-
-# print(pkts[9].show())
-
-# TO DO: Detect network "flows" among the sniffed packets.
-#       Flows are defined by the tuple (src IP address, src port, dest IP address, dest port, transport protocol).
-#       The transport protocol is either TCP or UDP.
-#       You can assign to each flow an ID, which is shared among all packets of that flow.
-
+# *****************
+# Begin Program!
+# *****************
+main()
