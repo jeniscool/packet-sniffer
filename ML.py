044319c526632007d9dc0b189f698832ac0e03ba
jeniscool <jmle265@g.uky.edu>
2019-04-12 19:23:16 -0400

    machine learning

--- ML.py
+++ ML.py
@@ -12,30 +12,32 @@ from sklearn import tree
 df = pd.read_csv("data.csv", header=None)
 # You might not need this next line if you do not care about losing information about flow_id etc. All you actually need to
 # feed your machine learning model are features and output label.
-columns_list = ['flow_id', 'IPsrc', 'IPdst', 'proto', 'feature_1', 'feature_2', 'feature_3', 'feature_4', 'feature_5', 'label']
+columns_list = ['flow_id', 'IPsrc', 'IPdst', 'proto', 'time', 'num_packets', 'sport', 'dport', 'avg_packet_size', 'label']
 df.columns = columns_list
-features = ['proto', 'feature_1', 'feature_2', 'feature_3', 'feature_4', 'feature_5']
+features = ['proto', 'time', 'num_packets', 'sport', 'dport', 'avg_packet_size']
 
-X = df[features]
-y = df['label']
+Features = df[features]
+Labels = df['label']
 
 acc_scores = 0
 for i in range(0, 10):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+    # Split the data set into training set and testing set
+    Features_train, Features_test, Labels_train, Labels_test = train_test_split(Features, Labels, test_size = 0.25)
 
     #Decision Trees
     clf = tree.DecisionTreeClassifier()
-    clf.fit(X_train, y_train)
+    clf.fit(Features_train, Labels_train)
 
     # Neural network (MultiPerceptron Classifier)
     # clf = MLPClassifier()
-    # clf.fit(X_train, y_train)
+    # clf.fit(Features_train, Labels_train)
 
     #SVM's
     # clf = SVC(gamma='auto')     #SVC USE THIS
     # clf = LinearSVC()  #Linear SVC
-    # clf.fit(X_train, y_train) 
+    # clf.fit(Features_train, Labels_train)
 
 
     #here you are supposed to calculate the evaluation measures indicated in the project proposal (accuracy, F-score etc)
-    result = clf.score(X_test, y_test)  #accuracy score
+    result = clf.score(Features_test, Labels_test)  #accuracy score
+    print(result)
